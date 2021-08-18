f3dc879a1f30696825c751d6940607586fafbe4d
kabilza <naruoluvohina@gmail.com>
2021-08-18 17:49:56 +0700

    done some landing page

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 def index(request):
     message = 'Welcome to index!'
     return render(request, 'mainapp/indexpage.html')
+
+def welcome(request):
+    return render(request, 'mainapp/welcomepage.html')
