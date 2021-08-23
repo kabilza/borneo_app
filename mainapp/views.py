ecc9c5390d0c6143e5bfe1cdc58ce93af596a137
kabilza <naruoluvohina@gmail.com>
2021-08-23 23:30:11 +0700

    make warranty resonsive

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 @login_required
 def index(request):
     return render(request, 'mainapp/index.html')
 
 def welcome(request):
-    return render(request, 'mainapp/welcomepage.html')
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('index'))
+    else:
+        return render(request, 'mainapp/welcomepage.html')
 
 def warranty_con(request):
     return render(request, 'mainapp/warrantycondition.html')
