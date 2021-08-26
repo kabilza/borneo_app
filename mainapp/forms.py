7b7f5018ac678d6e027ccebd2a05ff78352a6341
kabilza <naruoluvohina@gmail.com>
2021-08-26 23:34:52 +0700

    added some forms

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db.models.base import Model
 from django.forms import ModelForm
+from .models import BatteryWarranty, Profile
  
-class SignUpForm(UserCreationForm):
+class BatteryRegistrationForm(ModelForm):
     class Meta:
-        model = User
+        model = BatteryWarranty
         fields = ('username', 'password1', 'password2')
